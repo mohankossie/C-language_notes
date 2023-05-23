2a6e1c945fc0801bf15221f2d073861d07231720
mohankossie <mohankossie@gmail.com>
2023-05-23 11:23:11 +0000

    Structure of C

--- simple_c-program.c
+++ simple_c-program.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
-
+/*
 int main()   {
 	printf("Go to bed Moha!\n");
 	return 0; 
+}
+*/
+
+int integer_add(int x, int y)
+{
+	int result;
+	result = x + y;
+	return result;
+}
+
+int main()
+{
+	int sum;
+
+	sum = integer_add(3,7);
+	printf("The sum of 3 and 7 is %d.\n", sum);
+	return 0;
 }
