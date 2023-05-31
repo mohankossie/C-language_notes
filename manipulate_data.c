d258b9a631de868aee563ecd088b6c9a222d7df8
mohankossie <mohankossie@gmail.com>
2023-05-31 14:46:21 +0000

    Manipulating data

--- manipulate_data.c
+++ manipulate_data.c
@@ -35,6 +35,9 @@ int main()
 }
 */
 
+
+
+/*
 //Using pre- or post- increment(decrement) operators
 int main()
 {
@@ -57,3 +60,63 @@ int main()
 	
 	return 0;
 }
+*/
+
+
+//Relational operators
+int main()
+{
+	int x,y;
+	double z;
+
+	x = 7;
+	y = 25;
+	z = 24.46;
+	printf("Given x = %d, y = %d, and z = %.2f,\n", x, y, z);
+	printf("x >= y produces: %d\n", x >= y);
+	printf("x == y produces: %d\n", x == y);
+	printf("x < z produces: %d\n", x < z);
+	printf("y > z produces: %d\n", y > z);
+	printf("x != y -18 produces: %d\n", x != y - 18);
+	printf("x + y != z produces: %d\n", x + y != z);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
