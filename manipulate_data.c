7d56321c751ea88d97b16993cd0181481542042f
mohankossie <mohankossie@gmail.com>
2023-05-31 14:10:26 +0000

    Manipulating data

--- manipulate_data.c
+++ manipulate_data.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+/*
 //Using Arithmetic Operators
 int main()
 {
@@ -29,5 +31,29 @@ int main()
 	z *= x + y;
 	printf("z *= x +y assigns %d to z.\n", z);
 
+	return 0;
+}
+*/
+
+//Using pre- or post- increment(decrement) operators
+int main()
+{
+	int w, x, y, z, result;
+
+	w = x = y = z = 1; //initialize
+	printf("Given w = %d, x = %d, y = %d, and z = %d,\n", w, x, y, z);
+
+	result = ++w;
+	printf("++w evaluates to %d and w is now %d\n", result, w);
+
+	result = x++;
+	printf("x++ evaluates to %d and x is now %d\n", result, x);
+
+	result = --y;
+	printf("--y evaluates to %d and y is now %d\n", result, y);
+
+	result = z--;
+	printf("z-- evaluates to %d and z is now %d\n", result, z);
+	
 	return 0;
 }
