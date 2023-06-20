8d2905718ca0d7901a5458b5f2894b11868b8406
mohankossie <mohankossie@gmail.com>
2023-06-20 03:01:13 +0000

    Functions

--- functions.c
+++ functions.c
@@ -62,6 +62,7 @@ void GetDateTime(void)
 
 
 
+/*
 //Processing variable arguments
 double AddDouble (int x, ...);
 
@@ -97,6 +98,152 @@ double AddDouble (int x, ...)
 	va_end (arglist);
 	return result;
 }
+*/
+
+
+/*
+//Passing arrays to functions
+int AddThree(int list[]);
+
+int main()
+{
+	int sum, list[3];
+
+	printf("Enter three integers separated by spaces:\n");
+	scanf("%d%d%d", &list[0], &list[1], &list[2]);
+	sum = AddThree(list);
+	printf("The sum of the three integrs is: %d\n", sum);
+
+	return 0;
+
+}
+
+int AddThree(int list[])
+{
+	int i;
+	int result = 0;
+
+	for (i=0; i<3; i++)
+		result += list[i];
+	return result;
+}
+*/
+
+
+
+//Passing pointers to functions
+#include <stdio.h>
+
+void ChPrint(char *ch);
+int DataAdd(int *list, int max);
+
+int main()
+{
+	char str[] = "It's a string!";
+	char *ptr_str;
+	int list[5] = {1, 2, 3, 4, 5};
+	int *ptr_int;
+
+	//assign address to a pointer
+	ptr_str = str;
+	ChPrint(ptr_str);
+	ChPrint(str);
+
+	//assign address to a pointer
+	ptr_int = list;
+	printf("The sum returned by DataAdd(): %d\n", DataAdd(ptr_int, 5));
+	printf("The sum returned by DataAdd(): %d\n", DataAdd(list, 5));
+
+	return 0;
+}
+
+//function definition
+void ChPrint(char *ch)
+{
+	printf("%s\n", ch);
+}
+
+//function definition
+int DataAdd(int *list, int max)
+{
+	int i;
+	int sum = 0;
+
+	for (i=0; i<max; i++)
+		sum += list[i];
+	return sum;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
