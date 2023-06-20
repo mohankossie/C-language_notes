9eb7cf6f5fd3e084a53894594da1b4913342c6ac
mohankossie <mohankossie@gmail.com>
2023-06-20 08:21:57 +0000

    Functions

--- functions.c
+++ functions.c
@@ -131,6 +131,7 @@ int AddThree(int list[])
 
 
 
+/*
 //Passing pointers to functions
 #include <stdio.h>
 
@@ -173,11 +174,51 @@ int DataAdd(int *list, int max)
 		sum += list[i];
 	return sum;
 }
+*/
 
 
 
+//Passing multidimensional arrays to functions
+//function declarations
+int DataAdd1(int list[][5], int max1, int max2);
+int DataAdd2(int *list, int max1, int max2);
 
+main()
+{
+	int list[2][5] = {1,2,3,4,5,
+			  5,4,3,2,1};
+	int *ptr_int;
 
+	printf("The sum returned by DataAdd1(): %d\n", DataAdd1(list, 2, 5));
+	
+	ptr_int = &list[0][0];
+	printf("The sum returned by DataAdd2(): %d\n", DataAdd2(ptr_int, 2, 5));
+
+	return 0;
+}
+
+//function definitions
+int DataAdd1(int list[][5], int max1, int max2)
+{
+	int i, j;
+	int sum = 0;
+
+	for (i=0; i<max1; i++)
+		for (j=0; j<max2; j++)
+		sum += list[i][j];
+	return sum;
+}
+
+int DataAdd2(int *list, int max1, int max2)
+{
+	int i, j;
+	int sum =0;
+
+	for (i=0; i<max1; i++)
+		for (j=0; j<max2; j++)
+		sum += *(list + i*max2 +j);
+	return sum;
+}
 
 
 
